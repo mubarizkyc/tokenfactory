2b9f8721db3eb19ddd1e8da6cfe2c9928604cb2c
mubariz <mubarizharoonmulpk@gmail.com>
2024-07-05 16:01:37 +0500

    Imp remove delete

--- x/tokenfactory/module/simulation.go
+++ x/tokenfactory/module/simulation.go
@@ -90,17 +90,6 @@ func (am AppModule) WeightedOperations(simState module.SimulationState) []simtyp
 		tokenfactorysimulation.SimulateMsgUpdateDenom(am.accountKeeper, am.bankKeeper, am.keeper),
 	))
 
-	var weightMsgDeleteDenom int
-	simState.AppParams.GetOrGenerate(opWeightMsgDeleteDenom, &weightMsgDeleteDenom, nil,
-		func(_ *rand.Rand) {
-			weightMsgDeleteDenom = defaultWeightMsgDeleteDenom
-		},
-	)
-	operations = append(operations, simulation.NewWeightedOperation(
-		weightMsgDeleteDenom,
-		tokenfactorysimulation.SimulateMsgDeleteDenom(am.accountKeeper, am.bankKeeper, am.keeper),
-	))
-
 	// this line is used by starport scaffolding # simapp/module/operation
 
 	return operations
@@ -122,14 +111,6 @@ func (am AppModule) ProposalMsgs(simState module.SimulationState) []simtypes.Wei
 			defaultWeightMsgUpdateDenom,
 			func(r *rand.Rand, ctx sdk.Context, accs []simtypes.Account) sdk.Msg {
 				tokenfactorysimulation.SimulateMsgUpdateDenom(am.accountKeeper, am.bankKeeper, am.keeper)
-				return nil
-			},
-		),
-		simulation.NewWeightedProposalMsg(
-			opWeightMsgDeleteDenom,
-			defaultWeightMsgDeleteDenom,
-			func(r *rand.Rand, ctx sdk.Context, accs []simtypes.Account) sdk.Msg {
-				tokenfactorysimulation.SimulateMsgDeleteDenom(am.accountKeeper, am.bankKeeper, am.keeper)
 				return nil
 			},
 		),
